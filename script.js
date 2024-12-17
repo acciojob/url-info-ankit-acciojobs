7f8aac012a9a4c9f453467041c7cf803f021fc3d
ankit-acciojobs <ankitacciojob2000@gmail.com>
2024-12-17 17:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// Get the current URL using the location object
+const currentURL = window.location.href;
+alert("The length of the URL is: " + currentURL.length);
+
